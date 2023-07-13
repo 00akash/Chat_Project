b4c7aa2e9c6bb0a3b1ad53cb467cf26614e4da2e
AKASH KUMAR VARMA <akashbhujwa@gmail.com>
2023-07-13 10:52:59 +0530

    ChatPage MessageFormUI configuration Header created

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+   useMultiChatLogic,
+   MultiChatSocket,
+   MultiChatWindow,
+} from 'react-chat-engine-advanced';
+import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StanderdMessageForm";
+
+const Chat =()=>{
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "AkashChat",
+    "Akashchatengine@10"
+  )
+
+  return (
+     <div style={{ flexBasis: "100%"}}>
+    <MultiChatSocket {...chatProps} />
+    <MultiChatWindow
+      {...chatProps}
+      style={{ height: "100vh"}}
+      renderChatHeader={(chat)=> <Header chat={chat} />}
+      renderMessageForm={(props)=>{
+        return (
+          <StandardMessageForm props={props} activeChat={chatProps.chat} />
+        );
+      }}
+    />
+
+  </div>
+  )
+}
+
+export default Chat;
